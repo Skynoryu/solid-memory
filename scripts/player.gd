51f83ca5b1ec948df1990bb021e0ea83d5b22289
Leonardo Tada <leonardo_tada@hotmail.com>
2018-01-30 12:14:47 -0200

    Atualizado para Godot Engine 3.0

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@
 extends KinematicBody2D
 
 var id = 0
-var name = ""
+var player_name = ""
 export var hp = 100
 export var mp = 100
 export var moveSpeed = 5
@@ -11,23 +11,23 @@ export var meleeDamage = 10
 export var rangedDamage = 10
 
 func _ready():
-	set_fixed_process(true)
+	set_physics_process(true)
 	
-func _fixed_process(delta):
+func _physics_process(delta):
 	var left = Input.is_action_pressed("ui_left")
 	var right = Input.is_action_pressed("ui_right")
 	var up = Input.is_action_pressed("ui_up")
 	var down = Input.is_action_pressed("ui_down")
 	
 	if left:
-		move(Vector2(-moveSpeed, 0.0))
+		move_and_slide(Vector2(-moveSpeed, 0.0))
 	if right:
-		move(Vector2(moveSpeed, 0.0))
+		move_and_slide(Vector2(moveSpeed, 0.0))
 	if up:
-		move(Vector2(0.0, -moveSpeed))
+		move_and_slide(Vector2(0.0, -moveSpeed))
 	if down:
-		move(Vector2(0.0, moveSpeed))
+		move_and_slide(Vector2(0.0, moveSpeed))
 		
-	get_node("/root/Game/Camera").set_pos(self.get_pos())
+	get_node("/root/Game/Camera").position = self.position
 
 
